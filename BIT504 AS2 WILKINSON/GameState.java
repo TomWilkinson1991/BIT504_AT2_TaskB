e2dd3b83d2e6350adb523c354f044c9c02e11884
TomWilkinson1991 <113478758+TomWilkinson1991@users.noreply.github.com>
2022-09-15 23:00:27 +1200

    Initial Commit

---
+++ BIT504 AS2 WILKINSON/GameState.java
@@ -0,0 +1,6 @@
+package com.company;
+
+
+public enum GameState {
+    Playing, Draw, Cross_won,Nought_won
+}
---
+++ BIT504 AS2 WILKINSON/Player.java
@@ -0,0 +1,9 @@
+package com.company;
+
+/**
+ * Enumeration for the players move
+ */
+
+public enum Player {
+	Empty, Cross, Nought
+}

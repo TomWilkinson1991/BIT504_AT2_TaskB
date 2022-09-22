c304eb0f7b507d6d00e7824312fa0002d1685fc9
TomWilkinson1991 <113478758+TomWilkinson1991@users.noreply.github.com>
2022-09-22 17:13:25 +1200

    TODO comments removed from Cell class

--- BIT504 AS2 WILKINSON/Cell.java
+++ BIT504 AS2 WILKINSON/Cell.java
@@ -14,12 +14,12 @@ public class Cell {
 	/** Constructor to initialise this cell with the specified row and col */
 	public Cell(int row, int col) {
 		
-		// TODO: Initialise the variables row, col 
+		
 		this.col = col;
 		this.row = row;
 		
 		
-		//TODO: call the method that sets the cell content to EMPTY
+		
 		clear();
 	}
 	
@@ -48,7 +48,7 @@ public class Cell {
 	/** Set this cell's content to EMPTY */
 	public void clear() {
 		
-		// TODO: Set the value of content to Empty (Remember this is an enum)
+		
 		content = Player.Empty;
 		
 	}
